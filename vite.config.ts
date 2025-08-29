448fb0f4ef0a2359f23ebc7e3b38804eea79c95d
dheekshithgottiparthi <dheekshi0123@gmail.com>
2025-08-29 18:50:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // force correct base for GitHub Pages
+  base: '/portfolio/', // GitHub Pages repo name
 });
